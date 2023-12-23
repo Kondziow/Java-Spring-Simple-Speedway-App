d80f517afbd038c5716541c070c2dade0a43e852
Kondziow <konradwojtanowski02@gmail.com>
2023-12-23 17:53:43 +0100

    gateway init

---
+++ gateway-app/src/test/java/wojtanowski/konrad/gatewayapp/GatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package wojtanowski.konrad.gatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
