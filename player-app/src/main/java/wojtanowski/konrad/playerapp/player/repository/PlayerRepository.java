54c31fbaa888751338f78cb6283b47b913ba1522
Kondziow <konradwojtanowski02@gmail.com>
2023-12-25 19:11:38 +0100

    Player repo

---
+++ player-app/src/main/java/wojtanowski/konrad/playerapp/player/repository/PlayerRepository.java
@@ -0,0 +1,11 @@
+package wojtanowski.konrad.playerapp.player.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import wojtanowski.konrad.playerapp.player.model.entity.Player;
+
+import java.util.UUID;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, UUID> {
+}
