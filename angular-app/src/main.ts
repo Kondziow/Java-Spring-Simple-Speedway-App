ff826cc72308fccc0d8b15b32d96ba8576c37009
Kondziow <konradwojtanowski02@gmail.com>
2024-06-18 18:08:44 +0200

    feat: HttpClientModule add to import providers

--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -2,8 +2,9 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
 import {importProvidersFrom} from "@angular/core";
 import {AppRoutingModule} from "./app/app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(AppRoutingModule)]
+  providers: [importProvidersFrom(AppRoutingModule, HttpClientModule)]
 })
   .catch((err) => console.error(err));
