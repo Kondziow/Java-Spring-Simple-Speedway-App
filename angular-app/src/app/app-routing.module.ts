85c30b8e2a3418aea18e949143fba0cc0a78d6c5
Kondziow <konradwojtanowski02@gmail.com>
2024-06-18 18:08:11 +0200

    feat: add and set as default path - /clubs

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
+import {ClubListComponent} from "./club/club-list/club-list/club-list.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'clubs',
+    component: ClubListComponent
+  },
+  {
+    path: '',
+    redirectTo: 'clubs', pathMatch: 'full'
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
