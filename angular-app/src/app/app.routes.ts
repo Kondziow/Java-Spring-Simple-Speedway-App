ca1c817e2d3bdf3178ea08a20a844878323dfd6a
Kondziow <konradwojtanowski02@gmail.com>
2024-06-18 16:40:26 +0200

    new frontend initialized

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,20 +1,3 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from "@angular/core";
-import {ClubListComponent} from "./club/view/club-list/club-list.component";
-import {ClubDetailsComponent} from "./club/view/club-details/club-details.component";
-import {ClubEditComponent} from "./club/view/club-edit/club-edit.component";
-import {ClubAddComponent} from "./club/view/club-add/club-add.component";
-export const routes: Routes=  [
-  {path: 'clubs', component: ClubListComponent},
-  {path: 'clubs/add', component: ClubAddComponent},
-  {path: 'clubs/:id', component: ClubDetailsComponent},
-  {path: 'clubs/:id/edit', component: ClubEditComponent},
+import { Routes } from '@angular/router';
 
-  {path: '', redirectTo: 'clubs', pathMatch: 'full' }
-]
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutes{}
+export const routes: Routes = [];
--- angular-app/src/app/player/model/player.model.ts
+++
@@ -1,5 +0,0 @@
-import {ClubModel} from "../../club/model/club.model";
-
-export class PlayerModel {
-  constructor(public id: string, public name: string, public surname: string, public birthDate: string, public club: ClubModel) {}
-}
