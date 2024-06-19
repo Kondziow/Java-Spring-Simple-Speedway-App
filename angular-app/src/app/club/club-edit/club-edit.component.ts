45d4c5503ebbcff4185b99abea03d8d784517e98
Kondziow <konradwojtanowski02@gmail.com>
2024-06-19 09:40:35 +0200

    feat: add club form

--- angular-app/src/app/club/club-edit/club-edit.component.ts
+++ angular-app/src/app/club/club-edit/club-edit.component.ts
@@ -1,13 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ClubModel} from "../club.model";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ClubService} from "../club.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {relative} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-club-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './club-edit.component.html',
   styleUrl: './club-edit.component.css'
 })
-export class ClubEditComponent {
+export class ClubEditComponent implements OnInit {
+  clubId: string = '';
   club: ClubModel = new ClubModel("name", "city");
+  clubForm: FormGroup = new FormGroup({
+    'name': new FormControl('', Validators.required),
+    'city': new FormControl('', Validators.required)
+  });
+
+  constructor(private clubService: ClubService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.url.subscribe(urlSegments => {
+      this.clubId = this.activatedRoute.snapshot.params['id'];
+      this.initForm();
+    })
+  }
+
+  onSubmit() {
+    this.clubService.putClubById(this.club, this.clubId).subscribe(response => {
+      this.goBack();
+    })
+  }
+
+  goBack() {
+    this.router.navigate(['../..'], {relativeTo: this.activatedRoute})
+  }
+
+  initForm() {
+    let clubName = '';
+    let clubCity = '';
+
+    this.clubService.getClubById(this.clubId).subscribe(response => {
+      this.club = response;
+
+      clubName = this.club.name;
+      clubCity = this.club.city;
+
+      this.clubForm.patchValue({
+        'name': clubName,
+        'city': clubCity
+      })
+    })
+  }
 }
