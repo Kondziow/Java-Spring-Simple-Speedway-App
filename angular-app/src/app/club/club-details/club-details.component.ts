b7d6d82d0fa81acac21c23549add5b8539cfd703
Kondziow <konradwojtanowski02@gmail.com>
2024-06-20 18:46:55 +0200

    feat: loading club and players

--- angular-app/src/app/club/club-details/club-details.component.ts
+++ angular-app/src/app/club/club-details/club-details.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClubModel} from "../club.model";
+import {PlayerModel} from "../../player/player.model";
+import {ClubService} from "../club.service";
+import {PlayerService} from "../../player/player.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-club-details',
@@ -7,6 +12,28 @@ import { Component } from '@angular/core';
   templateUrl: './club-details.component.html',
   styleUrl: './club-details.component.css'
 })
-export class ClubDetailsComponent {
+export class ClubDetailsComponent implements OnInit{
+  clubId: string = '';
+  club?: ClubModel;
+  players?: PlayerModel[];
 
+  constructor(private clubService: ClubService,
+              private playerService: PlayerService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.clubId = params['id'];
+      this.clubService.getClubById(this.clubId).subscribe(response => {
+        this.club = response;
+        console.log(this.club)
+      })
+      this.playerService.getPlayersByClubId(this.clubId).subscribe(response => {
+        this.players = response;
+        console.log(this.players)
+      })
+    })
+  }
 }
