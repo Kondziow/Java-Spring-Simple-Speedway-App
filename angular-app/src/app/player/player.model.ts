619b658e5ee6562da9886487d54dc42c5cae4d04
Kondziow <konradwojtanowski02@gmail.com>
2024-06-20 17:57:38 +0200

    feat: add PlayerModel

---
+++ angular-app/src/app/player/player.model.ts
@@ -0,0 +1,7 @@
+export class PlayerModel {
+  constructor(public id: string,
+              public name: string,
+              public surname: string,
+              public birthDate: string) {
+  }
+}
