b40140dafed3206112e228d9db23f93b47f8cf6d
Kondziow <konradwojtanowski02@gmail.com>
2024-06-21 09:38:07 +0200

    feat:  path to adding new player

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from "@angular/core";
 import {ClubListComponent} from "./club/club-list/club-list.component";
 import {ClubEditComponent} from "./club/club-edit/club-edit.component";
 import {ClubDetailsComponent} from "./club/club-details/club-details.component";
+import {PlayerEditComponent} from "./player/player-edit/player-edit.component";
 
 
 const routes: Routes = [
@@ -24,6 +25,11 @@ const routes: Routes = [
   {
     path: 'clubs/:id/edit',
     component: ClubEditComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'clubs/:id/newPlayer',
+    component: PlayerEditComponent,
     pathMatch: 'full'
   },
   {
