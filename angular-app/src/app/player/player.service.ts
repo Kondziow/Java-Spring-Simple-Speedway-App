02dc98f0ccbcd746f3a7794d7bf33139c80dfa65
Kondziow <konradwojtanowski02@gmail.com>
2024-06-21 09:28:32 +0200

    feat: add PostNewPlayer model

--- angular-app/src/app/player/player.service.ts
+++ angular-app/src/app/player/player.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {backendUrl} from "../shared/http.config";
 import {ClubModel} from "../club/club.model";
-import {PlayerModel, PlayersResponse} from "./player.model";
+import {PlayerModel, PlayersResponse, PostPlayerRequest} from "./player.model";
 
 @Injectable({providedIn: 'root'})
 export class PlayerService {
@@ -12,5 +12,10 @@ export class PlayerService {
   getPlayersByClubId(clubId: string) {
     const url = `${backendUrl}/clubs/${clubId}/players`;
     return this.http.get<PlayersResponse>(url);
+  }
+
+  postNewPlayer(player: PostPlayerRequest) {
+    const url = `${backendUrl}/players`;
+    return this.http.post<PlayerModel>(url, player);
   }
 }
