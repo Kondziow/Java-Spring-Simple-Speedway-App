6e55a54809e01a6e45fdae030aa98377d9b70d27
Kondziow <konradwojtanowski02@gmail.com>
2024-06-21 08:49:40 +0200

    feat: initialize club-list and club-item

---
+++ angular-app/src/app/player/club-list/club-item/club-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-club-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './club-item.component.html',
+  styleUrl: './club-item.component.css'
+})
+export class ClubItemComponent {
+
+}
