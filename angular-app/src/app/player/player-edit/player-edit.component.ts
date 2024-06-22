4a316f8efe372ac75dda9ff2e78aea93c4882df5
Kondziow <konradwojtanowski02@gmail.com>
2024-06-22 12:45:27 +0200

    feat: editing player works

--- angular-app/src/app/player/player-edit/player-edit.component.ts
+++ angular-app/src/app/player/player-edit/player-edit.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgIf} from "@angular/common";
 import {PlayerModel, PostPlayerRequest} from "../player.model";
-import {ClubService} from "../../club/club.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {PlayerService} from "../player.service";
 
@@ -24,8 +23,9 @@ export class PlayerEditComponent implements OnInit {
   playerForm: FormGroup = new FormGroup({
     'name': new FormControl('', Validators.required),
     'surname': new FormControl('', Validators.required),
-    'birthday': new FormControl('')
+    'birthDate': new FormControl('')
   })
+  editMode: boolean  = false;
 
   constructor(private playerService: PlayerService,
               private activatedRoute: ActivatedRoute,
@@ -33,27 +33,71 @@ export class PlayerEditComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.activatedRoute.url.subscribe(urlSegments => {
+      this.editMode = urlSegments.some(segment => segment.path === 'edit');
+    });
+
     this.activatedRoute.url.subscribe(() => {
       this.clubId = this.activatedRoute.snapshot.params['id'];
     })
+
+    if (this.editMode) {
+      this.activatedRoute.url.subscribe(() => {
+        this.playerId = this.activatedRoute.snapshot.params['playerId'];
+        this.initForm();
+      })
+    }
   }
 
   onSubmit() {
-    let postPlayerRequest = new PostPlayerRequest(this.playerForm.value.name,
+    let postPlayerRequest = new PostPlayerRequest(
+      this.playerForm.value.name,
       this.playerForm.value.surname,
-      this.playerForm.value.birthday,
+      this.playerForm.value.birthDate,
       {
         id: this.clubId
       })
     console.log(this.playerForm.value)
     console.log(postPlayerRequest)
-    this.playerService.postNewPlayer(postPlayerRequest).subscribe(() => {
-      this.goBack()
-    })
+
+    if (this.editMode) {
+      this.playerService.putPlayerById(postPlayerRequest, this.playerId).subscribe(() => {
+        this.goBack()
+      })
+    } else {
+      this.playerService.postNewPlayer(postPlayerRequest).subscribe(() => {
+        this.goBack()
+      })
+    }
+
   }
 
   goBack() {
-    this.router.navigate(['..'], {relativeTo: this.activatedRoute})
+    if (this.editMode) {
+      this.router.navigate(['../../..'], {relativeTo: this.activatedRoute});
+    } else {
+      this.router.navigate(['..'], {relativeTo: this.activatedRoute});
+    }
+  }
+
+  initForm() {
+    let playerName = '';
+    let playerSurname = '';
+    let playerBirthDate = '';
+
+    this.playerService.getPlayerById(this.playerId).subscribe(response => {
+      this.player = response;
+
+      playerName = this.player.name;
+      playerSurname = this.player.surname;
+      playerBirthDate = this.player.birthDate;
+
+      this.playerForm.patchValue({
+        'name': playerName,
+        'surname': playerSurname,
+        'birthDate': playerBirthDate
+      })
+    })
   }
 
 }
